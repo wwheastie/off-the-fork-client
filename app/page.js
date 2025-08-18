e23cd889bb6f96ad25155a05ea0cc0a91bcc750f
Moe Deera <deeramoe@gmail.com>
2025-08-18 06:12:11 -0600

    committ

--- app/page.js
+++ app/page.js
@@ -20,9 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Testing:Save and see your changes instantly.
-          </li>
+          <li className="tracking-[-.01em]">Testing change time.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
