5cb5cd40015901cfaf7aba7ddefff9a67093804f
Moe Deera <deeramoe@gmail.com>
2025-10-06 22:23:10 -0600

    checkout page setup
    checkout page first stage. paypal installed.

--- app/cart/page.js
+++ app/cart/page.js
@@ -9,30 +9,40 @@ import YourDetailsComponent from "./YourDetailsComponent";
 import YourCartComponent from "./YourCartComponent";
 import { Button } from "@/components/ui/button";
 import { getCartTotal } from "../context/mockData";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function Page() {
   const { headerLinks, cartCount, cart, setCart, setCartCount } =
     useContext(GlobalContext);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="component-container">
-      <div className="cart-page-container">
-        <div>
-          <YourDetailsComponent />
-          <div className="hidden ml-6 w-4/5 md:flex flex-col gap-2 ">
-            <div className="flex justify-between font-bold  text-lg ">
-              <div>Total</div>
-              <div> $ {getCartTotal(cart)}</div>
-            </div>{" "}
-            <Button className="bg-orange-600  text-md w-full ">
-              Proceed To Payment
-            </Button>
+      {loading ? (
+        <div className="w-full border border-gray h-200 flex justify-center items-center text-lg">
+          <>
+            <Spinner /> <div className="m-200"></div>Loading
+          </>
+        </div>
+      ) : (
+        <div className="cart-page-container">
+          <div>
+            <YourDetailsComponent />
+            <div className="hidden ml-6 w-4/5 md:flex flex-col gap-2 ">
+              <div className="flex justify-between font-bold  text-lg ">
+                <div>Total</div>
+                <div> $ {getCartTotal(cart)}</div>
+              </div>{" "}
+              <Button className="bg-orange-600  text-md w-full ">
+                Proceed To Payment
+              </Button>
+            </div>
+          </div>
+          <div>
+            <YourCartComponent />
           </div>
         </div>
-        <div>
-          <YourCartComponent />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ app/checkout/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="page-container">
+      <div className="component-container border border-gray w-full h-full">
+        Checkout Page
+      </div>
+    </div>
+  );
+}
